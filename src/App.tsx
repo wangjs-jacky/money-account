9b6633faba0d1ed1280b95a729370c1103e8acff
所无聊 <2409277719@qq.com>
2022-05-01 13:49:40 +0800

    新增路由功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Hello World!
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/users" element={<Users></Users>}></Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+function Home(){
+  return(
+    <h2>Home</h2>
+  )
+}
+
+function About(){
+  return(
+    <h2>About</h2>
+  )
+}
+
+function Users(){
+  return(
+    <h2>Users</h2>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
